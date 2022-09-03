d7e5720b12de956900e925f541ec0868f9fa9e82
Duco Ritsema van Eck <ducorits@pop-os.localdomain>
2022-09-03 15:08:55 +0200

    Testing

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dritsema <dritsema@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/23 17:10:55 by dritsema      #+#    #+#                 */
-/*   Updated: 2022/08/30 17:54:46 by dritsema      ########   odam.nl         */
+/*   Updated: 2022/09/03 14:49:18 by dritsema      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,20 @@
 int	main(int argc, char **argv)
 {
 	int		len;
-	char *const	env[] = {"hoi\0"};
+	pid_t	id;
+	char *const	env[] = {NULL};
 
 	if (argc > 1)
 	{
 		len = ft_strlen(argv[1]);
 		write(1, argv[1], len);
 		write(1, "\n", 1);
-		execve("ls", &argv[1], env);
+		id = fork();
+		if (id == 0)
+		{
+			ft_printf("I am child\n");
+		}
+		execve("/bin/bash", argv, env);
 	}
 	return (0);
 }
